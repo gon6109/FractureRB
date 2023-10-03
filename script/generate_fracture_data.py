0fb723c01cbe4835e001c18bfa14e7351c996024
GON <hutabasuzukiryu@live.jp>
2023-10-03 18:17:56 +0900

    wip

--- script/generate_fracture_data.py
+++ script/generate_fracture_data.py
@@ -11,34 +11,43 @@ import re
 import pandas as pd
 from vtk.util import numpy_support
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../thirdparty/neural-fracture/src")
+sys.path.append(
+    os.path.dirname(os.path.abspath(__file__)) + "/../thirdparty/neural-fracture/src"
+)
 from dataset import shape
 from dataset import crack
+
 
 def start_index(args):
     res = 0
     for file in glob(f"{args.out}*_contact.*"):
-        i = int(re.match(fr"{args.out}(\d+)_contact\..*", file.replace("\\", "/")).groups()[0])
+        i = int(
+            re.match(rf"{args.out}(\d+)_contact\..*", file.replace("\\", "/")).groups()[
+                0
+            ]
+        )
         if i >= res:
             res = i + 1
 
     return res
+
 
 def compute_normal(triangle):
     normal = np.cross(triangle[1] - triangle[0], triangle[2] - triangle[0])
     normal /= np.linalg.norm(normal)
     return normal
 
+
 def is_inside_convex_hull(point, hull):
     hull_center = np.mean(hull.points, axis=0)
     for simplex in hull.simplices:
         triangle = hull.points[simplex]
         normal = compute_normal(triangle)
-        
+
         # 法線が外向きであることを保証
         if np.dot(normal, hull_center - triangle[0]) < 0:
             normal = -normal
-        
+
         # ポイントが面の外側にある場合
         if np.dot(normal, point - triangle[0]) < 0:
             return False
@@ -57,9 +66,8 @@ def generate_3d_convex_hull_obj(path, points):
         for v in points:
             f.write("v {} {} {}\n".format(v[0], v[1], v[2]))
         for face in faces:
-            f.write("f {} {} {}\n".format(
-                face[0] + 1, face[1] + 1, face[2] + 1))
-            
+            f.write("f {} {} {}\n".format(face[0] + 1, face[1] + 1, face[2] + 1))
+
 
 def generate_3d_shape_obj(path, points):
     # 凸包を計算
@@ -75,15 +83,16 @@ def generate_3d_shape_obj(path, points):
 
     # 頂点を重心方向に凹ます
     for index in hull.vertices:
-        points[index] += (centroid - points[index]) * min(np.random.exponential(0.25), 0.9)
+        points[index] += (centroid - points[index]) * min(
+            np.random.exponential(0.25), 0.9
+        )
 
     # objファイルとして出力
     with open(path, "w") as f:
         for v in points:
             f.write("v {} {} {}\n".format(v[0], v[1], v[2]))
         for face in faces:
-            f.write("f {} {} {}\n".format(
-                face[0] + 1, face[1] + 1, face[2] + 1))
+            f.write("f {} {} {}\n".format(face[0] + 1, face[1] + 1, face[2] + 1))
 
 
 def obj_to_voxel_and_sdf(path):
@@ -107,11 +116,15 @@ def obj_to_voxel_and_sdf(path):
     distanceFilter.Update()
 
     # Convert voxel to NumPy array
-    voxelArray = numpy_support.vtk_to_numpy(voxelGrid.GetOutput().GetPointData().GetScalars())
+    voxelArray = numpy_support.vtk_to_numpy(
+        voxelGrid.GetOutput().GetPointData().GetScalars()
+    )
     voxelArray = voxelArray.reshape((64, 64, 64))
 
     # Convert SDF to NumPy array
-    sdfArray = numpy_support.vtk_to_numpy(distanceFilter.GetOutput().GetPointData().GetScalars())
+    sdfArray = numpy_support.vtk_to_numpy(
+        distanceFilter.GetOutput().GetPointData().GetScalars()
+    )
     sdfArray = sdfArray.reshape((64, 64, 64))
 
     return voxelArray, sdfArray
@@ -120,7 +133,7 @@ def obj_to_voxel_and_sdf(path):
 def convert_data(root):
     for file in glob(f"{root}*_crack.csv"):
         try:
-            convert_path = os.path.splitext(file)[0] + f'.npz'
+            convert_path = os.path.splitext(file)[0] + f".npz"
             _crack = crack.read_crack(file)
             crack.save_crack_npz(convert_path, _crack)
             os.remove(file)
@@ -150,30 +163,39 @@ while start_index(parsed_args) < parsed_args.iter * parsed_args.shape:
         points_num = int(np.random.uniform(10, 30, size=(1))[0])
         points = np.random.uniform(-1, 1, size=(points_num, 3))
         generate_3d_shape_obj(
-            f"{parsed_args.out}{i * parsed_args.iter}_input.obj", points)
+            f"{parsed_args.out}{i * parsed_args.iter}_input.obj", points
+        )
         voxel, sdf = obj_to_voxel_and_sdf(
-            f"{parsed_args.out}{i * parsed_args.iter}_input.obj")
+            f"{parsed_args.out}{i * parsed_args.iter}_input.obj"
+        )
         for l in range(parsed_args.iter):
-            shape.save_shape_npz(f"{parsed_args.out}{i * parsed_args.iter + l}_voxel.npz", voxel)
-            shape.save_shape_npz(f"{parsed_args.out}{i * parsed_args.iter + l}_sdf.npz", sdf)
+            shape.save_shape_npz(
+                f"{parsed_args.out}{i * parsed_args.iter + l}_voxel.npz", voxel
+            )
+            shape.save_shape_npz(
+                f"{parsed_args.out}{i * parsed_args.iter + l}_sdf.npz", sdf
+            )
     offset = i * parsed_args.iter
     retry = 0
     while start_index(parsed_args) < parsed_args.iter * (i + 1):
         start = start_index(parsed_args) % parsed_args.iter
         print(f"start: {start}, offset:{offset}")
-        subprocess.run([
-            "GenerateFractureData.exe",
-            f"{parsed_args.out}{i * parsed_args.iter}_input.obj",
-            parsed_args.param,
-            "-o",
-            parsed_args.out,
-            "-i",
-            f"{parsed_args.iter}",
-            "--start",
-            f"{start}",
-            "--offset",
-            f"{offset}",
-        ])
+        subprocess.run(
+            [
+                "GenerateFractureData.exe",
+                f"{parsed_args.out}{i * parsed_args.iter}_input.obj",
+                parsed_args.param,
+                "-o",
+                parsed_args.out,
+                "-i",
+                f"{parsed_args.iter}",
+                "--start",
+                f"{start}",
+                "--offset",
+                f"{offset}",
+            ],
+            timeout=parsed_args.iter * 20,
+        )
         retry += 1
 
         if retry > 5:
@@ -184,4 +206,3 @@ while start_index(parsed_args) < parsed_args.iter * parsed_args.shape:
             retry = 0
 
     convert_data(parsed_args.out)
-    
